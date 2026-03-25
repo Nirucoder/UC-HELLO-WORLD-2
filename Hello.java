2c6145cf7280301fb77877a9d931ec0d84cb6e09
nirattay183@gmail.com <nirattay183@gmail.com>
2026-03-26 01:32:45 +0530

    Implement UC6: trailing delimiter substring and enhanced for loop

--- Hello.java
+++ Hello.java
@@ -1,11 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello World!");
+            System.out.println("Hello, World!");
         } else {
+            String names = "";
             for (String name : args) {
-                System.out.println("Hello " + name + "!");
+                names += name + ", ";
             }
+            names = names.substring(0, names.length() - 2);
+            System.out.println("Hello " + names + "!");
         }
     }
 }
